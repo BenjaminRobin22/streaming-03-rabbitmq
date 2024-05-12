b539b330ff2d5b0b6586e0aa32464bed1c92be71
Ben <Benjamin.Robin22@gmail.com>
2024-05-12 18:44:30 -0500

    committing changes to this project

--- emit_send.py
+++ emit_send.py
@@ -8,6 +8,6 @@ channel = connection.channel()
 
 channel.queue_declare(queue="hello")
 
-channel.basic_publish(exchange="", routing_key="hello", body="Hello World!")
-print(" [x] Sent 'Hello World!'")
+channel.basic_publish(exchange="", routing_key="hello", body="[x] This is Ben Robin's new message")
+print(" [x] This is Ben Robin's new message'")
 connection.close()
